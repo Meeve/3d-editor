1fae3a24c7cee37dc166fe6259dddb7c019c8b21
Daniel Wilkowski <daniel.wilkowski@callpage.io>
2018-08-04 14:37:27 +0200

    Extract cube definition to /mesh/cube.js (more...)
     - Clean numberField.js

--- src/components/numberField.js
+++ src/components/numberField.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { changeScale, addCube, selectMesh, changeMeshProp } from '../actions/index.js';
+import {bindActionCreators} from 'redux';
+import {changeScale, addCube, selectMesh, changeMeshProp} from '../actions/index.js';
 
 class NumberField extends Component {
     constructor(props) {
@@ -17,7 +17,7 @@ class NumberField extends Component {
             });
         });
     }
-    
+
     componentWillReceiveProps(nextProps) {
         this.setState({
             value: nextProps.selectedMesh[this.props.prop]
@@ -29,34 +29,36 @@ class NumberField extends Component {
             value: e.target.value
         });
 
-        if(!isNaN(e.target.value) && e.target.value !== "") {
+        if (!isNaN(e.target.value) && e.target.value !== "") {
             this.props.changeMeshProp(this.props.selectedMesh.id, this.props.prop, parseInt(e.target.value));
         }
     }
 
     mouseMove(e) {
-        if(this.state.mouseDown && this.state.isMouseIn) {
-            let offset = this.state.prevValue + ( + e.pageX - this.state.clickedX) / 20;
+        if (this.state.mouseDown && this.state.isMouseIn) {
+            let offset = this.state.prevValue + (+e.pageX - this.state.clickedX) / 20;
             this.setState({
                 value: offset
             });
-            this.props.changeMeshProp(this.props.selectedMesh.id, this.props.prop, offset );
+            this.props.changeMeshProp(this.props.selectedMesh.id, this.props.prop, offset);
         }
     }
 
     render() {
         return (
-        <div>
-            {this.state.prop}
-            <input 
-                onMouseDown={ e => {this.setState({ mouseDown: true, clickedX: e.pageX, prevValue: parseInt(this.state.value) });} } 
-                onMouseMove={ this.mouseMove.bind(this) }
-                onMouseLeave={ e => this.setState({ isMouseIn: false }) }
-                onMouseEnter={ e => this.setState({ isMouseIn: true }) }
-                onChange={ this.changeProp.bind(this)}
-                value={ this.state.value }
-                type="number"/> 
-        </div>
+            <div>
+                {this.state.prop}
+                <input
+                    onMouseDown={e => {
+                        this.setState({mouseDown: true, clickedX: e.pageX, prevValue: parseInt(this.state.value)});
+                    }}
+                    onMouseMove={this.mouseMove.bind(this)}
+                    onMouseLeave={e => this.setState({isMouseIn: false})}
+                    onMouseEnter={e => this.setState({isMouseIn: true})}
+                    onChange={this.changeProp.bind(this)}
+                    value={this.state.value}
+                    type="number"/>
+            </div>
         );
     }
 }
@@ -67,9 +69,9 @@ function mapStateToProps(state) {
     }
 }
 
-  function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ changeMeshProp }, dispatch);
-  }
-  
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(NumberField);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({changeMeshProp}, dispatch);
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NumberField);
--- src/reducers/allMeshes.js
+++ src/reducers/allMeshes.js
@@ -1,111 +1,31 @@
-function getCube() {
-    return {
-        vertices: [
-            // Przód
-            -1.0, -1.0, 1.0,
-            1.0, -1.0, 1.0,
-            1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
-
-            // Tył
-            -1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
-            1.0, 1.0, -1.0,
-            1.0, -1.0, -1.0,
-
-            // Góra
-            -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0,
-            1.0, 1.0, -1.0,
-
-            // Dół
-            -1.0, -1.0, -1.0,
-            1.0, -1.0, -1.0,
-            1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-
-            // Prawo
-            1.0, -1.0, -1.0,
-            1.0, 1.0, -1.0,
-            1.0, 1.0, 1.0,
-            1.0, -1.0, 1.0,
-
-            // Lewo
-            -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0
-        ],
-        // Dwa trójkąty dla każdej ze ścian
-        faces: [
-            0, 1, 2, 0, 2, 3, // Przednia ściana
-            4, 5, 6, 4, 6, 7, // Tylna ściana
-            8, 9, 10, 8, 10, 11, // Górna ściana
-            12, 13, 14, 12, 14, 15, // Dolna ściana
-            16, 17, 18, 16, 18, 19, // Prawa ściana
-            20, 21, 22, 20, 22, 23 // Lewa ściana
-        ],
-        colors: [
-            // Przód
-            0.5, 0.0, 0.0, 1.0,
-            0.5, 0.0, 0.0, 1.0,
-            0.5, 0.0, 0.0, 1.0,
-            0.5, 0.0, 0.0, 1.0,
-
-            // Tył
-            0.0, 0.5, 0.0, 1.0,
-            0.0, 0.5, 0.0, 1.0,
-            0.0, 0.5, 0.0, 1.0,
-            0.0, 0.5, 0.0, 1.0,
-
-            // Góra
-            0.0, 0.0, 0.5, 1.0,
-            0.0, 0.0, 0.5, 1.0,
-            0.0, 0.0, 0.5, 1.0,
-            0.0, 0.0, 0.5, 1.0,
-
-            // Dół
-            0.5, 0.0, 0.5, 1.0,
-            0.5, 0.0, 0.5, 1.0,
-            0.5, 0.0, 0.5, 1.0,
-            0.5, 0.0, 0.5, 1.0,
-
-            // Prawo
-            0.5, 5.0, 0.0, 1.0,
-            0.5, 5.0, 0.0, 1.0,
-            0.5, 5.0, 0.0, 1.0,
-            0.5, 5.0, 0.0, 1.0,
-
-            // Lewo
-            0.5, 0.5, 0.5, 1.0,
-            0.5, 0.5, 0.5, 1.0,
-            0.5, 0.5, 0.5, 1.0,
-            0.5, 0.5, 0.5, 1.0,
-        ],
-
-        vertexBuffer: null,
-        faceBuffer: null,
-        colorBuffer: null,
-
-        x: 1,
-        y: 0,
-        z: 1,
-
-        rx: 0,
-        ry: 0,
-        rz: 0
-    }
-}
+import createCube from './mesh/cube.js';
 
 export default (state = {}, action) => {
     switch (action.type) {
         case 'addCube':
-            let cube = getCube();
-            cube.id = Math.random();
-            return {[cube.id]: cube, ...state};
+            return addCube(state);
 
         case 'changeMeshProp': {
-            return Object.assign({}, state, {
-                [action.payload.id]: {
-                    ...state[action.payload.id],
-                    [action.payload.prop]: action.payload.value
-                }
-            });
+            return changeMeshProp(state, action);
         }
     }
     return state;
+}
+
+function addCube(state) {
+    let cube = createCube();
+    cube.id = Math.random();
+    return {
+        [cube.id]: cube,
+        ...state
+    };
+}
+
+function changeMeshProp(state, action) {
+    return Object.assign({}, state, {
+        [action.payload.id]: {
+            ...state[action.payload.id],
+            [action.payload.prop]: action.payload.value
+        }
+    });
 }
