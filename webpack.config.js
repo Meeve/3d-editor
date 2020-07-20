d639296796164ea315b58a67ed75ee872a5b6f48
Daniel Wilkowski <t.regx.rawr@gmail.com>
2020-07-20 20:43:52 +0200

    Update "webpack" to ^4.43.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  mode: 'development',
+  entry: './src/index.js',
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-env'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties'
+            ]
+          }
+        }
       }
-    }]
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
